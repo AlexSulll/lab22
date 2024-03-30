87b373fb96203a63b6de8a061f2d7466417b0b37
AlexSulll <hlebusheck.tv@gmail.com>
2024-03-30 18:43:19 +0300

    Готовый проект, остается исправить оформление

---
+++ src/app/entities/pipes/phone-format.pipe.ts
@@ -0,0 +1,14 @@
+import {Pipe, PipeTransform} from "@angular/core";
+
+
+@Pipe ({
+  name: 'phoneTransform',
+  standalone: true,
+  pure: true
+})
+
+export class PhoneFormatPipe implements PipeTransform {
+  transform(number: string): string {
+    return "+7(" + number.slice(0, 3) + ")" + number.slice(3, 6) + "-" + number.slice(6, 8) + "-" + number.slice(8);
+  }
+}
